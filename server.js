02ccd002390da95fad5ba6397a0a8f2380f4d18e
ritchey0713 <ritchey0713@gmail.com>
2018-09-26 13:28:18 -0400

    finish server.js

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+
+const app = express();
+
+//Bodyparser middleware
+app.use(bodyParser.json());
+
+// DB Config
+
+const db = require('./config/keys').mongoURI;
+
+// connect to mongoose
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err))
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server started on port ${port}`));
