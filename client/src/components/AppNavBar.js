cb7260cab0aba289ff1f8f3053d549ba5abe877e
ritchey0713 <ritchey0713@gmail.com>
2018-09-27 15:41:25 -0400

    build navbar

--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -16,6 +16,28 @@ class AppNavBar extends Component {
   }
 
   toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
+  render(){
+    <div>
+      <NavBar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href='/'>MERN Shopping</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/ritchey0713/MERN-shopping/tree/master/client/src" />
+                    GitHub
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+      </NavBar>
+    </div>
   }
 }
 
