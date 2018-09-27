2a2918f18c5587c17f25474daa49998f1a77018c
ritchey0713 <ritchey0713@gmail.com>
2018-09-27 15:33:31 -0400

    add navbar

---
+++ client/src/components/AppNavBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+ } from 'reactstrap';
+
+class AppNavBar extends Component {
+  state = {
+      isOpen: false
+  }
+
+  toggle = () => {
+  }
+}
+
+
+
+
+
+
+export default AppNavBar;
