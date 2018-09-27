a54d8fe9326cd3a5f0007868b5aedf0a1d95f440
ritchey0713 <ritchey0713@gmail.com>
2018-09-27 10:30:13 -0400

    set up delete action in routes

--- routes/API/items.js
+++ routes/API/items.js
@@ -8,7 +8,37 @@ const Item = require('../../models/Item');
 // @route GET api/items
 // @desc Get All Items
 // @access Public
-router.get('/');
+router.get('/', (request, response) => {
+  Item.find()
+  .sort({ date: -1 })
+    .then(items => response.json(items))
+    .catch(err => console.log(err))
+});
+
+// @route POST api/items
+// @desc Create A Item
+// @access Public
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save()
+  .then(item => res.json(item))
+  .catch(err => console.log(err))
+});
+
+// @route DELETE api/items
+// @desc Delete A Item
+// @access Public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove()
+    .then(() => res.json({ success: true})))
+    .catch(err => res.status(404).json({success: false }))
+    })
+
+
 
 
 module.exports = router;
